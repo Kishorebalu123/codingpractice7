0be5eecd0d72d0397240936ba853b16520405dc6
kishore <kishorebalueeda4@gmail.com>
2022-10-27 05:44:55 +0000

    code7a

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.get("/players/:playerId/playerScores", async (request, response) => {
     player_name AS playerName,
     SUM(score) AS totalScore,SUM(fours) AS totalFours,SUM(sixes) AS totalSixes
      FROM player_match_score NATURAL JOIN player_details 
-    WHERE player_id=${playerId}`;
+    WHERE player_id=${playerId};`;
   const playerStats = await database.get(getPlayersStatisticsQuery);
   response.send(playerStats);
 });
